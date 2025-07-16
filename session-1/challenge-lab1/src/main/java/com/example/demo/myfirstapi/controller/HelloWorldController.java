2680efd04cefeb8a3e4ae8a1a7c6b58c8e817630
sleep-deepfried <pulidoearljohn14@gmail.com>
2025-07-16 15:55:37 +0800

    Final Submission - Customizing Server & Application Properties

--- session-1/challenge-lab1/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
+++ session-1/challenge-lab1/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.demo.myfirstapi.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController //
 public class HelloWorldController {
     @GetMapping("/hello")
-    public String sayHello(){
-        return "Hello, Spring Boot Learners!";
+    public String sayHello() {
+        return customGreeting; // Now returns the custom message
     }
 
     @GetMapping("greet/{name}")
     public String greetUser(@PathVariable String name){
         return "Greetings," + name + "!";
     }
+
+    @Value("${app.greeting.message}") // Inject value from application.properties
+    private String customGreeting;
 }
